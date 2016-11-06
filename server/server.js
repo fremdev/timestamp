5ff2fd1a2d9a8d6aec2841ee5bb3694c9ea43130
Alex Efremov <alex.efremov.dev@gmail.com>
2016-11-06 13:55:10 +0200

    Add GET /:data route

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 const path = require('path');
 const express = require('express');
 
+const {getTimestampObj} = require('./utils/timestamp');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.static(publicPath));
+
+app.get('/:data', (req, res) => {
+  const data = req.params.data;
+  res.send(getTimestampObj(data));
+});
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
